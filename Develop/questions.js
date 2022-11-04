cc311f9971688c2ed7157003b3822acfd42ae006
BenLee <blee0963@gmail.com>
2022-11-04 00:04:23 -0700

    main

---
+++ Develop/questions.js
@@ -0,0 +1,53 @@
+let question = [
+    
+    {
+        question1: "What is 1 + 1 ?",
+        answer: "2",
+        options: [
+            "A: 1",
+            "B: 2",
+            "C: 3",
+            "D: 4",
+        ]
+    },
+    {
+        question2: "What is 2 + 2 ?",
+        answer: "4",
+        options: [
+            "A: 1",
+            "B: 2",
+            "C: 3",
+            "D: 4",
+        ]
+    },
+    {
+        question3: "What is 3 + 3 ?",
+        answer: "2",
+        options: [
+            "A: 2",
+            "B: 4",
+            "C: 6",
+            "D: 8",
+        ]
+    },
+    {
+        question4: "What is 4 + 4 ?",
+        answer: "2",
+        options: [
+            "A: 2",
+            "B: 4",
+            "C: 6",
+            "D: 8",
+        ]
+    },
+    {
+        question5: "What is 5 + 5 ?",
+        answer: "2",
+        options: [
+            "A: 10",
+            "B: 20",
+            "C: 30",
+            "D: 40",
+        ]
+    },
+];
---
+++ Develop/script.js
@@ -0,0 +1,6 @@
+const start_btn = document.querySelector(".start_btn button");
+const start_page = document.querySelector(".start_page button");
+const submit_btn = document.querySelector(".submit_btn button");
+// const start_btn = document.querySelector(".start_btn button");
+// const start_btn = document.querySelector(".start_btn button");
+// const start_btn = document.querySelector(".start_btn button");
